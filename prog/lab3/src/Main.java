adf52e6453f601a228dbbf6ce461e063dfcbb75b
alesulka <yarulinaalesya@gmail.com>
2024-12-06 17:55:00 +0300

    Add files via upload

--- prog/lab3/src/Main.java
+++ prog/lab3/src/Main.java
@@ -6,32 +6,29 @@ import java.time.LocalTime;
 
 public class Main {
     public static void main(String[] args) {
-        EntertainmentEstablishment e1 = new EntertainmentEstablishment("Веселый Балаганчик");
-        DepressingEstablishment e2 = new DepressingEstablishment("Депрессивный Балаганчик");
-        EntertainmentEstablishment e3 = new EntertainmentEstablishment("новое заведение");
-
-        e1.setOpeningTime(LocalTime.of(16,30));
-        e1.setClosingTime(LocalTime.of(22,30));
 
         Service service = new Service("Стендап Сергея Орлова", 5,"Новая программа","получает удовольствие и смеется от шуток комика");
-        e1.setService(service);
-
         Service service3 = new Service("попади в коротышку", 10,"кидает мяч ", "уворачивается от мяча ");
-        e3.setService(service3);
 
-        Street street = new Street("Невский проспект");
+        EntertainmentEstablishment e1 = new EntertainmentEstablishment("Веселый Балаганчик", 50, service, LocalTime.of(12,30),LocalTime.of(19,30) );
+        DepressingEstablishment e3 = new DepressingEstablishment("ITMO", 50, service3, LocalTime.of(12,30),LocalTime.of(19,30));
+
+
+        Street street = new Street("Кронвекский проспект");
         street.getInstitutions().add(e1);
-        street.getInstitutions().add(e2);
+        street.getInstitutions().add(e3);
 
-        Shorty shorty1 = new Shorty("Незнайка", 15, 10, Emotions.NEUTRAL);
-        Shorty shorty2 = new Shorty("Козлик", 14, 20, Emotions.NEUTRAL);
-        Shorty shorty3 = new Shorty("коротышка", 52, 10, Emotions.NEUTRAL);
-        Shorty shorty4 = new Shorty("Rich", 66, 100, Emotions.NEUTRAL);
+        Shorty shorty1 = new Shorty("Незнайка", 15, 10, Emotions.NEUTRAL);//незнайка
+        Shorty shorty2 = new Shorty("Козлик", 14, 20, Emotions.NEUTRAL);//козлик
+        Shorty shorty3 = new Shorty("первак", 52, 10, Emotions.NEUTRAL);//коротышка
+        Shorty shorty4 = new Shorty("Сергей Клименков", 66, 100, Emotions.NEUTRAL);
+
         e3.toHire(shorty3);
 
-        if (!e1.isWorking()){
+        if (!e1.isWorking() || !e3.isWorking()){
             shorty2.sayPhrase("ну ничего, скоро откроется");
         }
+
         while(shorty4.getMoney()>0){
             if (!shorty4.goToInstitution(e3)){
                 break;
@@ -40,13 +37,14 @@ public class Main {
 
         e3.employeeLeavePlace();
 
-
         shorty1.findWork(street);
         System.out.println("-----------------------------------");
+
         shorty1.goToInstitution(e1);
+        e1.checkWorkingTime(shorty1);
+
         shorty2.goToInstitution(e3);
-
-
+        e3.checkWorkingTime(shorty2);
 
     }
 }
