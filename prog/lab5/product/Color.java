0b5aaefc7b3da7e625e20a1c6786f178c81f5315
alesulka <yarulinaalesya@gmail.com>
2025-04-24 18:01:58 +0300

    Delete prog/lab5 directory

--- prog/lab5/product/Color.java
+++
@@ -1,7 +0,0 @@
-package org.example.product;
-
-public enum Color {
-    GREEN,
-    BLACK,
-    BROWN;
-}
--- prog/lab5/system/exception/WrongArgumentException.java
+++
@@ -1,7 +0,0 @@
-package org.example.system.exception;
-
-public class WrongArgumentException extends Exception{
-    public WrongArgumentException(String commandName) {
-        super("Wrong argument for command: " + commandName);
-    }
-}
